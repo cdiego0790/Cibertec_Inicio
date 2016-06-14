b2e85aa114c34cab31f34b9f54e266dcfac52b4e
cdiego0790 <cdiego0790@gmail.com>
2016-06-13 21:34:21 -0500

    Avance1

---
+++ WebDeveloper.DataAccess/ClientData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WebDeveloper.Model;
+using System.Linq;
+
+namespace WebDeveloper.DataAccess
+{
+    public class ClientData
+    {
+        public List<Client> GetFakeData()
+        {
+            return new List<Client>
+            {
+                new Client {ID=1, Name="Juan", LastName="Perez" },
+                new Client {ID=2, Name="Raul", LastName="Ruidiaz" }
+            };
+        }
+    }
+}

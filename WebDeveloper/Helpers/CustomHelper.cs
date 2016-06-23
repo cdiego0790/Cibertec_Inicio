13fb220bb5c55bfffe3edcfb10ddb377bad5104a
cdiego0790 <cdiego0790@gmail.com>
2016-06-22 22:10:14 -0500

    Manejo de Helpers. Optimizaciond e Codigos.

---
+++ WebDeveloper/Helpers/CustomHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDeveloper.Helpers
+{
+    public static class CustomHelper
+    {
+        public static IHtmlString DisplayPriceStatic(double price)
+        {
+            return new HtmlString(GetHtmlForPrice(price));
+        }
+
+        public static IHtmlString DisplayPriceExtension(this HtmlHelper helper, double price)
+        {
+            return new HtmlString(GetHtmlForPrice(price));
+        }
+
+        private static string GetHtmlForPrice(double price)
+        {
+            return price == 0.0 ? "<span>Free!!!</span>" : $"<span>{price.ToString()}</span>";
+        }
+
+        public static IHtmlString DisplayDateOrNull(this HtmlHelper helper, DateTime? registrationdate)
+        {
+            return new HtmlString(GetHtmlForDate(registrationdate));
+        }
+
+        private static string GetHtmlForDate(DateTime? registrationdate)
+        {
+            return registrationdate.HasValue ? $"<span>{registrationdate.Value.ToString("dd-mm-yyyy")}</span>" : "<span>None</span>";
+        }
+
+    }
+}

d57b28d6e2c0082074e2994f6a5f0feb4e5d62ae
cdiego0790 <cdiego0790@gmail.com>
2016-07-11 22:29:58 -0500

    Login Segurity admin

---
+++ WebDeveloper/Controllers/BaseAccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDeveloper.Controllers
+{
+    public class BaseAccountController : Controller
+    {
+        // GET: BaseAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
